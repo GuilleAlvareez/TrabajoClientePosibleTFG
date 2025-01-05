933c4f4264b5e791e63d053fe38cb75564a5e3bc
Guille_alvareez <guillealvarezmoreno2@gmail.com>
2025-01-05 19:48:51 +0100

    Base de datos creada a falta de pulir detalles

--- gymApp/admin.py
+++ gymApp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Usuario, Bloque, Semana, Dia, Ejercicio
 
 # Register your models here.
+admin.site.register(Usuario)
+admin.site.register(Bloque)
+admin.site.register(Semana)
+admin.site.register(Dia)
+admin.site.register(Ejercicio)
--- gymApp/models.py
+++ gymApp/models.py
@@ -1,3 +1,63 @@
 from django.db import models
 
 # Create your models here.
+
+class Usuario(models.Model):
+    nombre_usuario = models.CharField(max_length=60)
+    contraseña = models.CharField(max_length=50)
+    email = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name = 'Usuario'
+        verbose_name_plural = 'Usuarios'
+
+    def __str__(self):
+        return f"{self.nombre_usuario}: {self.contraseña}"
+
+class Bloque(models.Model):
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name = 'Bloque'
+        verbose_name_plural = 'Bloques'
+
+    def __str__(self):
+        return f"{self.usuario}: {self.nombre}"
+    
+class Semana(models.Model):
+    bloque = models.ForeignKey(Bloque, on_delete=models.CASCADE)
+    numero = models.IntegerField()
+
+    class Meta:
+        verbose_name = 'Semana'
+        verbose_name_plural = 'Semanas'
+
+    def __str__(self):
+        return f"{self.numero}"
+    
+class Dia(models.Model):
+    semana = models.ForeignKey(Semana, on_delete=models.CASCADE)
+    bloque = models.ForeignKey(Bloque, on_delete=models.CASCADE)
+    nombre_dia = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name = 'Dia'
+        verbose_name_plural = 'Dias'
+
+    def __str__(self):
+        return f"{self.semana.numero}: {self.nombre_dia}"
+    
+class Ejercicio(models.Model):
+    dia = models.ForeignKey(Dia, on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=100)
+    peso = models.IntegerField()
+    repeticiones = models.IntegerField()
+    observaciones = models.CharField(max_length=200)
+
+    class Meta:
+        verbose_name = 'Ejercicio'
+        verbose_name_plural = 'Ejercicios'
+
+    def __str__(self):
+        return f"{self.nombre}"
